fa71a9d53d35c6b5c5db5e0f091b3bf41f7a3788
Bengt Brodersen <bengt.brodersen@otto.de>
2020-03-02 10:38:42 +0100

    style: code format

--- zsh-lazyload.zsh
+++ zsh-lazyload.zsh
@@ -1,22 +1,22 @@
 function lazyload {  
-    local seperator='--'
-    local seperator_index=${@[(ie)$seperator]}
-    local cmd_list=(${@:1:(($seperator_index - 1))}); 
-    local load_cmd=${@[(($seperator_index + 1))]};    
-    if (( ${cmd_list[(I)${funcstack[2]}]} ))
-    then
-      unfunction $cmd_list
-      eval "$load_cmd"
-    else
-      local cmd
-      for cmd in $cmd_list
-      do
-        eval "function $cmd {
-          lazyload $cmd_list \
-            -- ${(qqqq)load_cmd}
-          $cmd \"\$@\"
-        }"
-      done
-    fi
+  local seperator='--'
+  local seperator_index=${@[(ie)$seperator]}
+  local cmd_list=(${@:1:(($seperator_index - 1))}); 
+  local load_cmd=${@[(($seperator_index + 1))]};    
+  if (( ${cmd_list[(I)${funcstack[2]}]} ))
+  then
+    unfunction $cmd_list
+    eval "$load_cmd"
+  else
+    local cmd
+    for cmd in $cmd_list
+    do
+      eval "function $cmd {
+        lazyload $cmd_list \
+          -- ${(qqqq)load_cmd}
+        $cmd \"\$@\"
+      }"
+    done
+  fi
 }
 
