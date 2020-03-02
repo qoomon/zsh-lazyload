bc76ed1a1b312b2ce4b7e18b2a1e4182a7d22b0e
Bengt Brodersen <bengt.brodersen@otto.de>
2020-03-02 10:45:27 +0100

    feat: handle missing load command

--- zsh-lazyload.zsh
+++ zsh-lazyload.zsh
@@ -2,7 +2,15 @@ function lazyload {
   local seperator='--'
   local seperator_index=${@[(ie)$seperator]}
   local cmd_list=(${@:1:(($seperator_index - 1))}); 
-  local load_cmd=${@[(($seperator_index + 1))]};    
+  local load_cmd=${@[(($seperator_index + 1))]};   
+   
+  if [[ ! $load_cmd ]]
+  then
+    >&2 echo "[ERROR] lazyload: No load command defined"
+    >&2 echo "  $@"
+    return 1
+  fi
+  
   if (( ${cmd_list[(I)${funcstack[2]}]} ))
   then
     unfunction $cmd_list
